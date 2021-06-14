a15ce30e7b398611f037a0d8671a4d5ee49f18ab
laraibamjad19 <82307735+laraibamjad19@users.noreply.github.com>
2021-06-14 21:03:01 +0500

    Add files via upload

---
+++ Controllers/SignUpController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace proj.Controllers
+{
+    public class SignUpController : Controller
+    {
+        // GET: SignUp
+        public ActionResult SignUp()
+        {
+            
+            return View();
+        }
+    }
+}
